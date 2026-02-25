ff8d512b7bb330af576cf96bc2c785cc09f2cf5f
DanielMartinez <danielmtzcastro202@gmail.com>
2026-02-24 18:01:37 -0600

    faltante de correciones en 01 app truthy y falsy

--- 08.5-Truthy y Falsy/js/01-app.js
+++ 08.5-Truthy y Falsy/js/01-app.js
@@ -1,4 +1,5 @@
-/* Truthy es un valor que se considera true/verdadero
+/* 
+Truthy es un valor que se considera true/verdadero
 cuando es evaluado bajo un contexto booleano, comportandose como true */
 
 const valTru = [true, 1, -1, "true", "false", "1", Infinity,"Symbol",'new Date()', '[]', '{}', '[1]', '[0]'];
---
+++ 08.5-Truthy y Falsy/js/03-app.js
@@ -0,0 +1,13 @@
+const falses = [[], 0, "",()=>{}, null,'1', '0',1, new Set(), new Map()]
+/*
+[]false
+0 false
+''false
+()=>{} true
+null false
+'1'true
+'0'true
+1 true
+new set() true
+new map() true
+*/
